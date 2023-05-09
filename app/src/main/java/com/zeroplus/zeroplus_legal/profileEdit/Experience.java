e2b7188c22a2774ecb77e34973b2ff6b1eb410b7
MAHMUDURRAHMANARNOB <mahmudurrahman2023@gmail.com>
2023-05-09 19:24:18 +0600

    updated professional--experience--educational--portfolio settings

--- app/src/main/java/com/zeroplus/zeroplus_legal/profileEdit/Experience.java
+++ app/src/main/java/com/zeroplus/zeroplus_legal/profileEdit/Experience.java
@@ -3,16 +3,29 @@ package com.zeroplus.zeroplus_legal.profileEdit;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.zeroplus.zeroplus_legal.ProfileSettingsActivity;
 import com.zeroplus.zeroplus_legal.R;
 
+import org.w3c.dom.Text;
+
 public class Experience extends AppCompatActivity {
+
+    TextView jDateView,lDateView;
+    LinearLayout LDate;
+    CheckBox CurrentyWorkHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +50,58 @@ public class Experience extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         /*Toolbar End*/
+
+        /*Joining Date*/
+        jDateView = (TextView) findViewById(R.id.jdateView);
+
+        jDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenDatePicker(jDateView);
+            }
+        });
+        /*Joining Date End*/
+
+        /*Leaving Date*/
+        lDateView = (TextView) findViewById(R.id.ldateView);
+        LDate = (LinearLayout) findViewById(R.id.lldate);
+
+        lDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenDatePicker(lDateView);
+            }
+        });
+        /*Leaving Date End*/
+
+        CurrentyWorkHere = (CheckBox) findViewById(R.id.checkBoxCWH);
+
+        CurrentyWorkHere.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(CurrentyWorkHere.isChecked()){
+                    LDate.setVisibility(View.GONE);
+                }
+                else{
+                    LDate.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+
+    }
+
+    private void OpenDatePicker(TextView Where_To_Set){
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String PickedDate = String.valueOf(day)+"-"+(month+1)+"-"+(year);
+                Log.d("PickedDate",PickedDate);
+                Where_To_Set.setText(PickedDate);
+            }
+        }, 2023, 0, 15);
+
+        dialog.show();
     }
 }
--- app/src/main/java/com/zeroplus/zeroplus_legal/profileEdit/Professional.java
+++ app/src/main/java/com/zeroplus/zeroplus_legal/profileEdit/Professional.java
@@ -6,13 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.zeroplus.zeroplus_legal.ProfileSettingsActivity;
 import com.zeroplus.zeroplus_legal.R;
 
+import java.lang.reflect.Array;
+
 public class Professional extends AppCompatActivity {
+
+    ArrayAdapter<String> arrayAdapter;
+    AutoCompleteTextView ACTV_gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,23 @@ public class Professional extends AppCompatActivity {
         setSupportActionBar(toolbar);
         /*Toolbar End*/
 
+//        ACTV_gender = (AutoCompleteTextView) findViewById(R.id.ACTV_gender);
+//
+//        String[] gender = getResources().getStringArray(R.array.gender);
+//        arrayAdapter = new ArrayAdapter<>(this,R.layout.gender_dropdown_item,gender);
+//        ACTV_gender.setAdapter(arrayAdapter);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ACTV_gender = (AutoCompleteTextView) findViewById(R.id.ACTV_gender);
+
+        String[] gender = getResources().getStringArray(R.array.gender);
+        arrayAdapter = new ArrayAdapter<>(this,R.layout.gender_dropdown_item,gender);
+        ACTV_gender.setAdapter(arrayAdapter);
 
     }
 }
