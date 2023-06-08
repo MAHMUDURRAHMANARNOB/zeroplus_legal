a4fb1e5a83b2991bea697774ab2ccdcebd28e45a
MAHMUDURRAHMANARNOB <mahmudurrahman2023@gmail.com>
2023-06-08 17:44:18 +0600

    ui update

--- app/src/main/java/com/zeroplus/zeroplus_legal/First2Fragment.java
+++ app/src/main/java/com/zeroplus/zeroplus_legal/First2Fragment.java
@@ -19,6 +19,7 @@ public class First2Fragment extends Fragment {
 
     SharedPreferences prf;
     private FragmentFirst2Binding binding;
+    NavigationBarView bottombar;
 
     @Override
     public View onCreateView(
@@ -35,76 +36,42 @@ public class First2Fragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         prf = getActivity().getSharedPreferences(SessionManager.SESSION_REMEMBERME,0);
 
-        /*binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(First2Fragment.this)
-                        .navigate(R.id.action_First2Fragment_to_SecondFragment);
-            }
-        });*/
-        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+
+        bottombar = (NavigationBarView) view.findViewById(R.id.bottom_navigation);
+        bottombar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment;
                 switch (item.getItemId()) {
 
                     case R.id.itemEarnings:
-                        /*fragment = fragmentHome;*/
                         fragment = new EarningsFragment();
                         FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
                         ft.replace(R.id.mainFrame, fragment);
                         ft.commit();
                         break;
                     case R.id.itemhome:
-                        /*fragment = fragmentClass;*/
                         fragment = new DashboardFragment();
                         FragmentTransaction ft1 = getActivity().getSupportFragmentManager().beginTransaction();
                         ft1.replace(R.id.mainFrame, fragment);
                         ft1.commit();
                         break;
-
-                    /*case R.id.itemReferrals:
-                        *//*fragment = fragmentClass;*//*
-                        fragment = new ReferralsFragment();
-                        FragmentTransaction ft2 = getActivity().getSupportFragmentManager().beginTransaction();
-                        ft2.replace(R.id.mainFrame, fragment);
-                        ft2.commit();
-                        break;*/
-
                     case R.id.itemProfile:
-                        /*fragment = fragmentClass;*/
                         fragment = new ProfileFragment();
                         FragmentTransaction ft3 = getActivity().getSupportFragmentManager().beginTransaction();
                         ft3.replace(R.id.mainFrame, fragment);
                         ft3.commit();
                         break;
-
-                    /*case R.id.itemlogout:
-
-                        SharedPreferences.Editor editor = prf.edit();
-                        editor.clear();
-                        editor.commit();
-
-                        Intent intent = new Intent(getActivity(),
-                                MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                        getActivity().finish();
-
-                        *//*Intent intent = new Intent(getActivity(), MainActivity.class);
-                        startActivity(intent);
-                        getActivity().finish();*//*
-                        break;*/
-
                     default:
-                        /* fragment = fragmentHome;*/
 
                 }
-//                fragmentManager.beginTransaction().replace(R.id.mainFrame, fragment).commit();
+
                 return true; // return true;
             }
         });
-        binding.bottomNavigation.setSelectedItemId(R.id.itemhome);
+        bottombar.setSelectedItemId(R.id.itemhome);
+
     }
 
     @Override
